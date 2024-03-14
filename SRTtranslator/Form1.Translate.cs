bee6cfffbf510e5060c62e6eeab596c5234cd649
Maximus Polo <black3snake@mail.ru>
2024-03-14 17:03:28 +0800

    Nlog, BlockingCollection, AsParallel

--- SRTtranslator/Form1.Translate.cs
+++ SRTtranslator/Form1.Translate.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Net.Http;
 using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SRTtranslator
@@ -13,50 +15,150 @@ namespace SRTtranslator
     public partial class Form1
     {
         static readonly string srtWord = @"^[^\d{1,}]\w.*$";
+        object locker = new object();
+        ParallelOptions options = new ParallelOptions();
+
+        static BlockingCollection<MyClass> bc = new BlockingCollection<MyClass>();
+
 
         private static HttpClient Client = new HttpClient()
         {
             BaseAddress = new Uri("https://translate.googleapis.com")
         };
 
-        internal void TranslatorHub(List<string> listSelectedFiles)
+        internal async void TranslatorHub(List<string> listSelectedFiles)
         {
             Dictionary<string, string> dict = new Dictionary<string, string>();
             Dictionary<string, string> dictO2 = new Dictionary<string, string>();
-            
-            foreach (string fileName in listSelectedFiles)
+
+            //options.MaxDegreeOfParallelism = Environment.ProcessorCount > 2 ? Environment.ProcessorCount - 1 : 1;
+            ConsoleTB.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+
+            await Zapusk(listSelectedFiles);
+
+            OutputC(bc);
+
+            //listSelectedFiles.AsParallel().WithDegreeOfParallelism(options.MaxDegreeOfParallelism).ForAll(ls => { MyTask(ls); });
+
+
+            /*foreach (string fileName in listSelectedFiles)
             {
                 dict = DictSringFromFile(fileName);
 
                 Task<Dictionary<string, string>> taskDic = Task.Run<Dictionary<string, string>>(() => TaskTanslateString3(dict));
+                //                Task<Dictionary<string, string>> taskDic = new Task<Dictionary<string, string>>(TaskTanslateString2, dict);
+                //Task<Dictionary<string, string>> taskDic2 = new Task<Dictionary<string, string>>(() => TaskTanslateString3(dict));
+                //taskDic2.Start();
+                //Task.Run<Dictionary<string, string>>(() => TaskTanslateString3(dict));
 
                 taskDic.ContinueWith((t) =>
                 {
-                    foreach (var te in t.Result)
+                    lock (locker)
                     {
+                        foreach (var te in t.Result)
+                        {
+                            Invoke(new Action(() =>
+                            {
+                                ConsoleTB.AppendText(te.Key + Environment.NewLine);
+                                ConsoleTB.AppendText(te.Value + Environment.NewLine);
+                                ConsoleTB.AppendText(Environment.NewLine);
+                            }));
+                        }
+
                         Invoke(new Action(() =>
                         {
-                            ConsoleTB.AppendText(te.Key + Environment.NewLine);
-                            ConsoleTB.AppendText(te.Value + Environment.NewLine);
-                            ConsoleTB.AppendText(Environment.NewLine);
+                            if (ConsoleTB.TextLength > 500 && ConsoleTB.ScrollBars != System.Windows.Forms.ScrollBars.Vertical)
+                            {
+                                ConsoleTB.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+                                ConsoleTB.AppendText(ConsoleTB.TextLength + Environment.NewLine);
+                            }
+                        }));
+
+                        Invoke(new Action(() =>
+                        {
+                            ConsoleTB.AppendText($"Task Id ended : {Task.CurrentId}" + Environment.NewLine);
+                            ConsoleTB.AppendText($"Thread Id ended : {Thread.CurrentThread.ManagedThreadId}" + Environment.NewLine);
+                            ConsoleTB.AppendText($"файл: {fileName}");
                         }));
                     }
-
-                    Invoke(new Action(() =>
-                    {
-                        if (ConsoleTB.TextLength > 500 && ConsoleTB.ScrollBars != System.Windows.Forms.ScrollBars.Vertical)
-                        {
-                            ConsoleTB.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-                            ConsoleTB.AppendText(ConsoleTB.TextLength + Environment.NewLine);
-                        }
-                    }));
-                });
-            }
+                }, TaskContinuationOptions.AttachedToParent); ;
+            }*/
         }
 
+        internal Task Zapusk(List<string> listSelectedFiles)
+        {
+            options.MaxDegreeOfParallelism = Environment.ProcessorCount > 2 ? Environment.ProcessorCount - 1 : 1;
+
+            listSelectedFiles.AsParallel().WithDegreeOfParallelism(options.MaxDegreeOfParallelism).ForAll(ls => { MyTask(ls); });
+
+            return Task.CompletedTask;
+        }
+
+        internal void MyTask(object arg)
+        {
+            string ft = (string)arg;
+
+            logger.Info($"MyTask: CurrentId {Task.CurrentId} with ManagedThreadId {Thread.CurrentThread.ManagedThreadId} запущен, имя файла {ft}" + Environment.NewLine);
+
+            var dict = DictSringFromFile(ft);
+            var resultT = TaskTanslateString3(dict);
+
+            MyClass myClass = new MyClass(ft, resultT.Result);
+            bc.TryAdd(myClass);
+
+            #region Random
+            /*var random = new Random();
+            var lowerBound = 3000;
+            var upperBound = 7000;
+            var rNum = random.Next(lowerBound, upperBound);
+
+            Thread.Sleep(rNum);*/
+            #endregion
+
+            logger.Info($"MyTask: CurrentId " + Task.CurrentId + " завершен." + Environment.NewLine);
+        }
+
+        internal void OutputC(BlockingCollection<MyClass> bc)
+        {
+            foreach (var item in bc)
+            {
+                ConsoleTB.AppendText(item.FileName + Environment.NewLine);
+                foreach (var te in item.Dict)
+                {
+                    ConsoleTB.AppendText(te.Key + Environment.NewLine);
+                    ConsoleTB.AppendText(te.Value + Environment.NewLine);
+                    ConsoleTB.AppendText(Environment.NewLine);
+
+                }
+
+                ConsoleTB.AppendText($"Конец файла: {item.FileName}" + Environment.NewLine);
+                ConsoleTB.AppendText(Environment.NewLine);
+            }
+
+            if (ConsoleTB.TextLength > 500)
+            {
+                ConsoleTB.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+                ConsoleTB.AppendText(ConsoleTB.TextLength + Environment.NewLine);
+            }
+
+        }
+
+
+        internal Dictionary<string, string> TaskTanslateString2(Dictionary<string, string> dict)
+        {
+            //Dictionary<string, string> dict2 = new Dictionary<string, string>();
+            return dict;
+        }
+        
+       
         internal async Task<Dictionary<string, string>> TaskTanslateString3(Dictionary<string, string> dict)
         {
             Dictionary<string, string> dict2 = new Dictionary<string, string>();
+            /*Invoke(new Action(() =>
+            {
+                ConsoleTB.AppendText($"Task Id метода TaskTanslateString3 : {Task.CurrentId}" + Environment.NewLine);
+                ConsoleTB.AppendText($"Thread Id метода DTaskTanslateString3 : {Thread.CurrentThread.ManagedThreadId}" + Environment.NewLine);
+            }));*/
 
             foreach (var d in dict.Keys)
             {
@@ -80,10 +182,10 @@ namespace SRTtranslator
                     }
                 }
 
-                if (ConsoleTB.TextLength > 500)
+                /*if (ConsoleTB.TextLength > 500)
                 {
                     ConsoleTB.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-                }
+                }*/
             }
 
 
@@ -91,5 +193,17 @@ namespace SRTtranslator
         }
 
 
+    }
+
+    internal class MyClass
+    {
+        public string FileName;
+        public Dictionary<string, string> Dict;
+
+        public MyClass(string fileName, Dictionary<string, string> dict)
+        {
+            this.FileName = fileName;
+            this.Dict = dict;
+        }
     }
 }
--- SRTtranslator/Form1.cs
+++ SRTtranslator/Form1.cs
@@ -8,13 +8,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NLog;
+using NLog.Targets;
+using NLog.Config;
+using System.Web.UI.WebControls;
 
 namespace SRTtranslator
 {
     public partial class Form1 : Form
     {
         List<string> listSelectFiles = new List<string>();
-
+        static Logger logger = LogManager.GetCurrentClassLogger();
 
         public Form1()
         {
@@ -38,10 +42,22 @@ namespace SRTtranslator
             {
                 if(listView1.SelectedItems.Count > 1)
                 {
+                    if (ConsoleTB.TextLength > 0)
+                        ConsoleTB.Clear();
+
+                    if (bc.Count > 0)
+                    {
+                        //MyClass localItem;
+                        while (bc.TryTake(out MyClass localItem)) {}
+                    }
+                        
                     foreach (ListViewItem lVitem in listView1.SelectedItems)
                     {
                         listSelectFiles.Add(labDir.Text + "\\" + lVitem.Text);
                     }
+                    TranslatorHub(listSelectFiles);
+                    listSelectFiles.Clear();
+                    
 
                 } else if (listView1.SelectedItems.Count == 0)
                 {
@@ -49,14 +65,12 @@ namespace SRTtranslator
                     return;
                 } else
                 {
-                    if(listSelectFiles.Count > 0)
-                        listSelectFiles.Clear();
-                    
                     if (ConsoleTB.TextLength > 0)
                         ConsoleTB.Clear();
 
                     listSelectFiles.Add(labDir.Text + "\\"+ listView1.SelectedItems[0].Text);
                     TranslatorHub(listSelectFiles);
+                    listSelectFiles.Clear();
                 }
 
 
@@ -65,6 +79,41 @@ namespace SRTtranslator
 
 
             listView1.SmallImageList = imageList1;
+
+            #region NLog Initializator
+
+            var config = new NLog.Config.LoggingConfiguration();
+            LogManager.Configuration = new LoggingConfiguration();
+            const string LayoutFile = @"[${date:format=yyyy-MM-dd HH\:mm\:ss}] [${logger}/${uppercase: ${level}}] [THREAD: ${threadid}] >> ${message}
+${exception: format=ToString}";
+
+            var logfile = new FileTarget();
+
+            if (!Directory.Exists("logs"))
+                Directory.CreateDirectory("logs");
+
+            // Rules for mapping loggers to targets
+            config.AddRule(LogLevel.Trace, LogLevel.Fatal, logfile);
+
+            DateTime dtlog = DateTime.Now;
+
+            logfile.CreateDirs = true;
+            logfile.FileName = $"logs{Path.DirectorySeparatorChar}{dtlog:yyyy-MM-dd}.log";
+            logfile.AutoFlush = true;
+            logfile.LineEnding = LineEndingMode.CRLF;
+            logfile.Layout = LayoutFile;
+            logfile.FileNameKind = FilePathKind.Absolute;
+            logfile.ConcurrentWrites = false;
+            logfile.KeepFileOpen = false;
+
+
+            // Apply config
+            NLog.LogManager.Configuration = config;
+
+            #endregion NLog Initializator
+
+            logger.Info($"!!!---- Start Main Program {DateTime.Now}");
+
 
         }
 
